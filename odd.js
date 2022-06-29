6ce4ff8e680154a1fc24862ebb7f0ba02aa6ac9b
Jeffrey <jeff.ho@hotmail.com>
2022-06-29 21:35:09 +0000

    Odd checker assignment

---
+++ odd.js
@@ -0,0 +1,11 @@
+function is0dd(num) {
+  if (num % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log("3 is odd: " + is0dd(3));
+console.log("8 is odd: " + is0dd(8));
+
